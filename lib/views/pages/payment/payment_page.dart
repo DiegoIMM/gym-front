e106cbafca877b23eca872c0094906dc04804f1b
Diego Martinez <diegomartinezpavez@icloud.com>
2023-11-20 11:29:34 -0300

    se agrega vista responsiva en pagos

--- lib/views/pages/payment/payment_page.dart
+++ lib/views/pages/payment/payment_page.dart
@@ -32,139 +32,154 @@ class _PlanPageState extends State<PaymentPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Pagos',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    var result = await showDialog<dynamic>(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return const PaymentForm();
-                      },
-                    );
-                    if (result) {
-                      getPayments();
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Pagos',
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      var result = await showDialog<dynamic>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return const PaymentForm();
+                        },
+                      );
+                      if (result) {
+                        getPayments();
+                      }
+                    },
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.add,
+                          color: Colors.purple,
+                        ),
+                        Text('Nuevo pago',
+                            style: TextStyle(color: Colors.purple)),
+                      ],
+                    ))
+              ],
+            ),
+            FutureBuilder(
+              future: futurePayments,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return const Text('No hay conexión al servidor');
+                  case ConnectionState.active:
+                  case ConnectionState.waiting:
+                    return LoadingWidget(text: 'Cargando preguntas');
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
                     }
-                  },
-                  child: const Row(
-                    children: [
-                      Icon(
-                        Icons.add,
-                        color: Colors.purple,
-                      ),
-                      Text('Nuevo pago',
-                          style: TextStyle(color: Colors.purple)),
-                    ],
-                  ))
-            ],
-          ),
-          FutureBuilder(
-            future: futurePayments,
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return const Text('No hay conexión al servidor');
-                case ConnectionState.active:
-                case ConnectionState.waiting:
-                  return LoadingWidget(text: 'Cargando preguntas');
-                case ConnectionState.done:
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  }
 
-                  if (!snapshot.hasData) {
-                    return NoData();
-                  }
+                    if (!snapshot.hasData) {
+                      return NoData();
+                    }
 
-                  if (snapshot.hasData) {
-                    var width = MediaQuery.of(context).size.width;
-                    var payments = snapshot.data!;
+                    if (snapshot.hasData) {
+                      var width = MediaQuery.of(context).size.width;
+                      var payments = snapshot.data!;
 
-                    return payments.isEmpty
-                        ? NoData()
-                        : SingleChildScrollView(
-                            child: Column(children: [
-                            Padding(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: (width > 1000) ? 8.0 : 0.0,
-                                  horizontal: (width > 1000) ? 50.0 : 8.0,
-                                ),
-                                child: Row(children: [
-                                  Expanded(
-                                    child: DataTable(
-                                      columns: const <DataColumn>[
-                                        DataColumn(
-                                          label: Text(
-                                            'Fecha',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
+                      return payments.isEmpty
+                          ? NoData()
+                          : Column(children: [
+                              Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: (width > 1000) ? 8.0 : 0.0,
+                                    horizontal: (width > 1000) ? 50.0 : 8.0,
+                                  ),
+                                  child: Row(children: [
+                                    Expanded(
+                                        child: Center(
+                                            child: SingleChildScrollView(
+                                      scrollDirection: Axis.horizontal,
+                                      child: DataTable(
+                                        columns: const <DataColumn>[
+                                          DataColumn(
+                                            label: Text(
+                                              'Fecha',
+                                              style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.w900,
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                        DataColumn(
-                                          label: Text(
-                                            'Cliente',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
+                                          DataColumn(
+                                            label: Text(
+                                              'Cliente',
+                                              style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.w900,
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                        DataColumn(
-                                          label: Text(
-                                            'Plan',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
+                                          DataColumn(
+                                            label: Text(
+                                              'Plan',
+                                              style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.w900,
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                        DataColumn(
-                                          label: Text(
-                                            'Expira',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
+                                          DataColumn(
+                                            label: Text(
+                                              'Expira',
+                                              style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.w900,
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                        DataColumn(
-                                          label: Text(
-                                            'Pagado',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic),
+                                          DataColumn(
+                                            label: Text(
+                                              'Pagado',
+                                              style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.w900,
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                      rows: [
-                                        for (var payment in payments)
-                                          DataRow(
-                                            cells: <DataCell>[
-                                              DataCell(Text(payment.date
-                                                  .toString()
-                                                  .substring(0, 10))),
-                                              DataCell(Text(
-                                                  '${payment.client.name} ${payment.client.rut}')),
-                                              DataCell(Text(payment.plan.name)),
-                                              DataCell(Text(payment.expiredAt
-                                                  .toString()
-                                                  .substring(0, 10))),
-                                              DataCell(Text(
-                                                  payment.price.toString())),
-                                            ],
-                                          ),
-                                      ],
-                                    ),
-                                  )
-                                ]))
-                          ]));
-                  } else {
-                    return const Text('No hay datos');
-                  }
-              }
-            },
-          ),
-        ]));
+                                        ],
+                                        rows: [
+                                          for (var payment in payments)
+                                            DataRow(
+                                              cells: <DataCell>[
+                                                DataCell(Text(payment.date
+                                                    .toString()
+                                                    .substring(0, 10))),
+                                                DataCell(Text(
+                                                    '${payment.client.name} ${payment.client.rut}')),
+                                                DataCell(
+                                                    Text(payment.plan.name)),
+                                                DataCell(Text(payment.expiredAt
+                                                    .toString()
+                                                    .substring(0, 10))),
+                                                DataCell(Text(
+                                                    payment.price.toString())),
+                                              ],
+                                            ),
+                                        ],
+                                      ),
+                                    )))
+                                  ]))
+                            ]);
+                    } else {
+                      return const Text('No hay datos');
+                    }
+                }
+              },
+            ),
+          ])),
+    );
   }
 }
